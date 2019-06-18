d5ed1e34ec6599a86d681ec822a4d5897c9ce777
cavu757 <jose.pehovazdiez@gmail.com>
2019-06-18 01:11:18 -0400

    Gemfile and create tables

--- db/migrate/20190618045912_create_bplans.rb
+++ db/migrate/20190618045912_create_bplans.rb
@@ -1,7 +1,15 @@
 class CreateBplans < ActiveRecord::Migration
   def change
     create_table :bplans do |t|
-
+      t.text :problem
+      t.text :solution
+      t.text :key_metrics
+      t.text :cost_structure
+      t.text :unique_value_proposition
+      t.text :unfair_advantage
+      t.text :channels
+      t.text :customer_segments
+      t.text :revenue_streams
       t.timestamps null: false
     end
   end
